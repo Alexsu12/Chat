1a39545375c06411fd4dea27a472b4f2adec7b63
Alexsu12 <asanurb1205@g.educaand.es>
2022-04-27 14:38:57 +0200

    Crea envio y Recepcion

--- src/main/java/utilidades/Envio.java
+++ src/main/java/utilidades/Envio.java
@@ -1,7 +1,19 @@
 package utilidades;
 
-public class Envio {
-    public static void enviar(String mensaje) {
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class Envio {
+    public static void enviar(String mensaje, ArrayList<Socket> clientes) {
+        PrintStream envio;
+        for (Socket cliente : clientes) {
+            try {
+                envio = new PrintStream(cliente.getOutputStream());
+                envio.println(mensaje);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/utilidades/Recepcion.java
+++ src/main/java/utilidades/Recepcion.java
@@ -1,12 +1,34 @@
 package utilidades;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class Recepcion {
-    private String recepcion(Socket cliente) {
-        return null;
+    private String recepcionHost(Socket cliente) {
+        BufferedReader recibe;
+        String mensaje = "";
+        try {
+            recibe = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            mensaje = recibe.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mensaje;
     }
 
+    private String recepcionCliente(Socket cliente) {
+        BufferedReader recibe;
+        String mensaje = "";
+        try {
+            recibe = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            mensaje = recibe.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mensaje;
+    }
     public static void recepcionClientes() {
 
     }
